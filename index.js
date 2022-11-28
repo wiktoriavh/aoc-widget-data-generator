8097e382e3568e87bceeed4bc300744f3fd6260c
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2022-11-28 20:46:14 +0100

    use different schema for repo name
    instead of href, use username/reponame

--- index.js
+++ index.js
@@ -2,9 +2,26 @@ const core = require("@actions/core");
 
 const YEAR = core.getInput("year");
 const LANGUAGE = core.getInput("language");
-const REPO_HREF = core.getInput("repo_href");
+const REPO = core.getInput("repo");
 const DIRECTORY = core.getInput("directory");
 const OUTPUT = core.getInput("output");
 const EXTENSION = core.getInput("extension");
 
-console.log({ YEAR, LANGUAGE, REPO_HREF, REPO_HREF, OUTPUT, EXTENSION });
+const getFilename = (day, extensions) => {
+  return `${day}.${extensions}`;
+};
+
+const getPath = (directory, filename) => {
+  return `${directory}/${filename}`;
+};
+
+const repo = "Braweria/advent-of-code-2015-go";
+const ext = "go";
+const directory = "days";
+
+const getFullPath = (repo, dir) => {
+  // https://api.github.com/repos/Braweria/advent-of-code-2015-go/contents/days
+  return `https://api.github.com/repos/${repo}/content/${dir}`;
+};
+
+console.log(getFullPath(REPO, DIRECTORY));
