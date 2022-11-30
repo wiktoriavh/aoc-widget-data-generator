6db4709f021348dc87f9432bc12b5ee86655f712
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2022-11-30 18:46:44 +0100

    implement esbuild

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import core from "@actions/core";
+import fetch from "node-fetch";
+
+const YEAR = core.getInput("year");
+const LANGUAGE = core.getInput("language");
+const REPO = core.getInput("repo");
+const DIRECTORY = core.getInput("directory");
+const OUTPUT = core.getInput("output");
+const EXTENSION = core.getInput("extension");
+
+const getFilename = (day, extensions) => {
+  return `${day}.${extensions}`;
+};
+
+const getPath = (directory, filename) => {
+  return `${directory}/${filename}`;
+};
+
+const repo = "Braweria/advent-of-code-2015-go";
+const ext = "go";
+const directory = "days";
+
+const getFullPath = (repo, dir) => {
+  return `https://api.github.com/repos/${repo}/contents/${dir}`;
+};
+
+const getFiles = async (path) => {
+  const response = await fetch(path);
+  const data = await response.json();
+  return data;
+};
+
+console.log(getFiles(getFullPath(REPO, DIRECTORY)));
